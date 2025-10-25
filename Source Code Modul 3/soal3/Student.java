b8bc396f6c95edc4657cf55d93dc68c7177cef35
Muhammad Irgi Fahrezha <2410817210005@mhs.ulm.ac.id>
2025-10-25 20:16:32 +0800

    Modul 3

---
+++ Source Code Modul 3/soal3/Student.java
@@ -0,0 +1,19 @@
+package praktikum3.soal3;
+
+class Student {
+    private String name;
+    private String studentId;
+
+    public Student(String name, String studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+}
